f2a47df2d99af7a869eae461377518e84781bb67
kevinkirui2 <smoothdime@gmail.com>
2025-07-21 01:19:29 +0300

    Slight Changes

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -2,31 +2,42 @@ import './landing.css';
 import Navbar from "./Navbar";
 import Projects from "./Projects";
 import FadeInSection2 from './FadeInSection2';
-function Landing(){
+import logo from "./Images/logo2.png";
 
-    return(
-      
-        <div>
-        <div id='wallpaper' className="wallpaper">
-           
-         <FadeInSection2>
-            <h1 className=' w-full text-5xl font-bold ml-3 text-white pt-40 text-center'>
-            <span className='font-bold text-lg w-1/2  text-white pt-40 text-center'>Hi, I'm Kevin Kirui</span><br></br><br></br>Focused on interfaces and experiences, working remotely from Nairobi, Kenya.</h1>
-             <div className="pt-10  text-center">
-            <button className="bg-green-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded"> <a href="https://1drv.ms/b/c/9273c3e03a8e1704/EUaIMB_0i6tOtXWAO9OfRGkBzZSrWU_1lSvy4aRTcKZL9A?e=QdBe73"> View Resume </a></button>
-           
-        </div>
+function Landing() {
+  return (
+    <div>
+      <div id="wallpaper" className="wallpaper">
+        <FadeInSection2>
+          <div className="pt-40 text-center">
+            {/* Centered Logo */}
+            <img src={logo} className="w-24 mx-auto rounded" alt="logo" />
+            
+            {/* Heading & Description */}
+            <h1 className="text-5xl font-bold text-white mt-6">
+              Hi, I'm Kevin Kirui
+            </h1>
+            <p className="text-lg text-white mt-4 w-full max-w-2xl mx-auto">
+              Focused on interfaces and experiences, working remotely from Nairobi, Kenya.
+            </p>
+
+            {/* Resume Button */}
+            <div className="pt-10">
+              <a
+                href="https://1drv.ms/b/c/9273c3e03a8e1704/EUaIMB_0i6tOtXWAO9OfRGkBzZSrWU_1lSvy4aRTcKZL9A?e=QdBe73"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="bg-green-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded">
+                  View Resume
+                </button>
+              </a>
+            </div>
+          </div>
         </FadeInSection2>
-        </div>
-
-        <div>
-
-        
-        </div>
-        
-        </div>
-    )
-
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
